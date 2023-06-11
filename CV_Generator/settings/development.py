88e478cc060a56b08fcd7efd6081e9b6596f9a16
miclemabasie <miclemabasie3@gmail.com>
2023-06-11 02:15:48 +0100

    Database config

--- CV_Generator/settings/development.py
+++ CV_Generator/settings/development.py
@@ -1,12 +1,12 @@
 from .base import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'your_db_name',
-#         'USER': 'postgres',
-#         'PASSWORD': 'your_db_password',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
