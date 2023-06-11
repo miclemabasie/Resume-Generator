76ecc19c0566283261de6bda820520a5f37dbfaa
Miclem <miclemabasie3@gmail.com>
2023-06-11 21:21:12 +0100

    home page and templates setup

--- cvs/views.py
+++ cvs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import (
+    CV,
+    PersonalInfomation,
+    Education,
+    Experience,
+    Skill,
+    Language,
+    Contact,
+    Project,
+    Achievement,
+)
 
-# Create your views here.
+
+def home(request):
+    template_name = "home.html"
+
+    context = {
+        "section": "home",
+    }
+    return render(request, template_name, context)
