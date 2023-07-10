60d2126a228f057b51fc7f60d98568c0d10aa32e
miclemabasie <miclemabasie3@gmail.com>
2023-07-10 09:23:02 +0100

    updated settings for pythonanywhere

--- CV_Generator/settings/base.py
+++ CV_Generator/settings/base.py
@@ -20,7 +20,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 DEBUG = True
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
 SECRET_KEY = "dkalfjalkdsjfalksdjflkasjdflkasjdfkl;ajsdfl;kasjdflka;j"
 
 
@@ -168,42 +168,42 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-import logging
-import logging.config
-from django.utils.log import DEFAULT_LOGGING
+# import logging
+# import logging.config
+# from django.utils.log import DEFAULT_LOGGING
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-LOG_LEVEL = "INFO"
+# LOG_LEVEL = "INFO"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
-            },
-            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
-            },
-            "file": {
-                "level": "INFO",
-                "class": "logging.FileHandler",
-                "formatter": "file",
-                "filename": "cv_logs/cv.log",
-            },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
-            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-        },
-    }
-)
+# logging.config.dictConfig(
+#     {
+#         "version": 1,
+#         "disable_existing_loggers": False,
+#         "formatters": {
+#             "console": {
+#                 "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+#             },
+#             "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+#             "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+#         },
+#         "handlers": {
+#             "console": {
+#                 "class": "logging.StreamHandler",
+#                 "formatter": "console",
+#             },
+#             "file": {
+#                 "level": "INFO",
+#                 "class": "logging.FileHandler",
+#                 "formatter": "file",
+#                 "filename": "cv_logs/cv.log",
+#             },
+#             "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+#         },
+#         "loggers": {
+#             "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+#             "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+#             "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+#         },
+#     }
+# )
